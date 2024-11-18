040d17d3166673cb30512039fcd1d8b676b2f4a5
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-19 01:34:02 +0500

    Add project files.

---
+++ Tyuiu.KurbanovFA.Sprint5.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Tyuiu.KurbanovFA.Sprint5.Task0.V21.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint5.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            string path = @"C:\Users\Cruise\source\repos\Tyuiu.KurbanovFA.Sprint5\Tyuiu.KurbanovFA.Sprint5.Task0.V21\bin\Debug\net8.0OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
