9bd4092b34ef0d8b402f7f2be0f9298ca44efc67
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-26 01:53:39 +0500

    Task7

---
+++ Tyuiu.KurbanovFA.Sprint5.Task7.V4.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KurbanovFA.Sprint5.Task7.V4.Lib
+{
+    public class DataService : ISprint5Task7V4
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathTransfer = File.ReadAllText(path);
+            string stringValues = pathTransfer;
+
+            string result = "";
+
+            foreach (char c in stringValues)
+            {
+                // Проверяем, является ли символ русской буквой
+                if ((c >= 'А' && c <= 'Я') || (c >= 'а' && c <= 'я'))
+                {
+                    // Заменяем русскую букву на '#'
+                    result += '#';
+                }
+                else
+                {
+                    // Если символ не русская буква, оставляем его как есть
+                    result += c;
+                }
+            }
+            string outputFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V4");
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                writer.WriteLine(result);
+            }
+            return outputFile;
+        }
+    }
+}
---
+++ Tyuiu.KurbanovFA.Sprint5.Task7.V4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KurbanovFA.Sprint5.Task7.V4.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint5.Task7.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Курбанов Ф.А. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Курбанов Фархаджон Азамжанович | РППб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Заменить все русские буквы на #                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\Users\Cruise\AppData\Local\Temp\InPutDataFileTask7V4";
+            string res = ds.LoadDataAndSave(path);
+            Console.WriteLine("Результат: " + res);
+            Console.ReadLine();
+        }
+    }
+}
