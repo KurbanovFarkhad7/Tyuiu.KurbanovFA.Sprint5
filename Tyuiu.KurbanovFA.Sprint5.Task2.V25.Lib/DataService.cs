34290eda14ab74c566b75603a85f51a043b19a82
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-25 20:50:29 +0500

    Task2

--- Tyuiu.KurbanovFA.Sprint5.Task2.V25.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint5.Task2.V25.Lib/DataService.cs
@@ -7,33 +7,32 @@ namespace Tyuiu.KurbanovFA.Sprint5.Task2.V25.Lib
     {
         public string SaveToFileTextData(int[,] matrix)
         {
-            string outputFile = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
-            using (StreamWriter writer = new StreamWriter(outputFile))
-            {
-                for (int y = 0; y < matrix.GetLength(0); y++)
-                {
-                    for (int x = 0; x < matrix.GetLength(1); x++)
-                    {
-                        if (matrix[y, x] % 2 != 0)
-                        {
-                            x = 0;
-                            writer.Write(x + "\t");
-                        }
-                        else
-                        {
-                            writer.WriteLine(x + "\t");
-                        }
-                    }
-                }
-            }
-
             for (int y = 0; y < matrix.GetLength(0); y++)
             {
                 for (int x = 0; x < matrix.GetLength(1); x++)
                 {
-                    Console.Write(matrix[y, x] + "\t");
+                    if (matrix[y, x] % 2 != 0)
+                    {
+                        matrix[y, x] = 0;
+                        Console.Write(matrix[y, x] + "\t");
+                    }
+                    else
+                    {
+                        Console.Write(matrix[y, x] + "\t");
+                    }
                 }
                 Console.WriteLine();
+            }
+            string outputFile = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                for (int y = 0;y < matrix.GetLength(0); y++)
+                {
+                    for (int x = 0; x < matrix.GetLength(1); x++)
+                    {
+                        writer.Write(matrix[y,x] + "\t");
+                    }
+                }
             }
             return outputFile;
         }
