cca9f05443d05f4b29d73d688cf0e09a3522e73f
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-25 23:25:07 +0500

    Task4

--- Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib/DataService.cs
@@ -6,21 +6,11 @@ namespace Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            double result = 0;
-            if (File.Exists(path))
-            {
-                try
-                {
-                    string pathTransfer = File.ReadAllText(path);
-                    double x = double.Parse(pathTransfer);
-                    result = Math.Round((4.26 * x) / (Math.Sin(x)), 3);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Ошибка при обработке файла или вычислениях: {ex.Message}");
-                }
-            }
-            return result;
+            string tempDirectory = Path.GetTempPath();
+            path = Path.Combine(tempDirectory, "InPutDataFileTask4V5.txt");
+            string pathTransfer = File.ReadAllText(path);
+            double x = double.Parse(pathTransfer);
+            return Math.Round((4.26 * x) / (Math.Sin(x)), 3);
         }
     }
 }
