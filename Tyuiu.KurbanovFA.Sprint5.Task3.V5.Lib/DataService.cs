81e039480ef42be72e6900fed6acf0dbbff62c30
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-25 22:15:51 +0500

    Task3

--- Tyuiu.KurbanovFA.Sprint5.Task3.V5.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint5.Task3.V5.Lib/DataService.cs
@@ -7,7 +7,10 @@ namespace Tyuiu.KurbanovFA.Sprint5.Task3.V5.Lib
         public string SaveToFileTextData(int x)
         {
             string outputFile = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
-            using (StreamWriter writer = new StreamWriter(outputFile))
+            string filePath = Path.GetTempFileName();
+            filePath = Path.ChangeExtension(filePath, "bin");
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            using (BinaryWriter writer = new BinaryWriter(fs))
             {
                 writer.Write(2 * Math.Pow(x, 3) + 0.5 * Math.Pow(x, 2) - 3.5 * x + 2);
 
