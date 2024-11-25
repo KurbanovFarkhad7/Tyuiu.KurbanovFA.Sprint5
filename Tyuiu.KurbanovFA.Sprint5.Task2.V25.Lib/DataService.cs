a8cee635e27ae0c2b2ae2fc8e8ad7d5372727a6a
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-25 21:37:53 +0500

    Task2

--- Tyuiu.KurbanovFA.Sprint5.Task2.V25.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint5.Task2.V25.Lib/DataService.cs
@@ -11,8 +11,7 @@ namespace Tyuiu.KurbanovFA.Sprint5.Task2.V25.Lib
             {
                 for (int x = 0; x < matrix.GetLength(1); x++)
                 {
-                    Console.Write(matrix[y, x] + 1000);
-                    /*if (matrix[y, x] % 2 != 0)
+                    if (matrix[y, x] % 2 != 0)
                     {
                         matrix[y, x] = 0;
                         Console.Write(matrix[y, x]);
@@ -24,24 +23,24 @@ namespace Tyuiu.KurbanovFA.Sprint5.Task2.V25.Lib
                     if (x < 2)
                     {
                         Console.Write(";");
-                    }*/
+                    }
                 }
-                /*Console.WriteLine();*/
+                Console.WriteLine();
             }
-            string outputFile = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
+            string outputFile = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
             using (StreamWriter writer = new StreamWriter(outputFile))
             {
                 for (int y = 0;y < matrix.GetLength(0); y++)
                 {
                     for (int x = 0; x < matrix.GetLength(1); x++)
                     {
-                        writer.Write(matrix[y,x] + 100000);
+                        writer.Write(matrix[y,x]);
                         if (x < 2)
                         {
                             writer.Write(";");
                         }
                     }
-                    /*writer.WriteLine();*/
+                    writer.WriteLine();
                 }
             }
             return outputFile;
