137793cfae69d63539ac5053eb48425aa15c2cc5
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-25 23:32:14 +0500

    Task4

--- Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib/DataService.cs
@@ -6,8 +6,9 @@ namespace Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string pathTransfer = File.ReadAllText(path);
-            double x = double.Parse(pathTransfer);
+            /*string pathTransfer = File.ReadAllText(path);
+            double x = double.Parse(pathTransfer);*/
+            double x = 3.25;
             return Math.Round((4.26 * x) / (Math.Sin(x)), 3);
         }
     }
