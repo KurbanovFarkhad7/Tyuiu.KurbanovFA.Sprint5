43ec963fd0422a9a78c28525b6ee3032db175b55
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-25 23:19:40 +0500

    Task4

--- Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib/DataService.cs
@@ -6,9 +6,21 @@ namespace Tyuiu.KurbanovFA.Sprint5.Task4.V5.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string pathTransfer = File.ReadAllText(path);
-            double x = double.Parse(pathTransfer);
-            return Math.Round((4.26 * x) / (Math.Sin(x)), 3);
+            double result = 0;
+            if (File.Exists(path))
+            {
+                try
+                {
+                    string pathTransfer = File.ReadAllText(path);
+                    double x = double.Parse(pathTransfer);
+                    result = Math.Round((4.26 * x) / (Math.Sin(x)), 3);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при обработке файла или вычислениях: {ex.Message}");
+                }
+            }
+            return result;
         }
     }
 }
